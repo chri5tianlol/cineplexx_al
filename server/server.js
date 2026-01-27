6f613c0120143ec181629e5f230a47e848930517
Kristjan <kristjankullolli8888@gmail.com>
2026-01-27 20:11:27 +0100

    Allow CORS from all domains

--- server/server.js
+++ server/server.js
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 5000;
 
 const path = require('path');
 
-app.use(cors());
+app.use(cors({
+    origin: '*', // Allow all origins (simplest for now)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads'))); // Serve uploads
 
